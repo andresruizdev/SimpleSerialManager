19cf29dd583402a5c8034b4da47c7c40547e6311
Andres Ruiz <rb.andres98@gmail.com>
2022-10-18 14:14:16 -0500

    Initialize Project
    * Project Created
    * Add Support to get Available Serial Ports

---
+++ SimpleSerialManager/SimplePort.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Ports;
+
+namespace SimpleSerialManager
+{
+    public class SimplePort
+    {
+        SerialPort _serialPort = new SerialPort();
+
+        public static string[] GetAvailablePorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public static List<string> GetAvailablePortsList()
+        {
+            return SerialPort.GetPortNames().ToList();
+        }
+    }
+}
