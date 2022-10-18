fd64f8678fcd254748d4690eb55c5ffca2e209e0
Andres Ruiz <rb.andres98@gmail.com>
2022-10-18 16:02:01 -0500

    Add Open and Close method
    * Add method to open port
    * Add method to check if port is open
    * Add method to close port

--- SimpleSerialManager/SimplePort.cs
+++ SimpleSerialManager/SimplePort.cs
@@ -15,5 +15,35 @@ namespace SimpleSerialManager
         {
             return SerialPort.GetPortNames().ToList();
         }
+
+        public bool Open(string portName, int bauds)
+        {
+            var avaiblePortsList = GetAvailablePortsList();
+            if (avaiblePortsList.Contains(portName)){
+                _serialPort.PortName = portName;
+                _serialPort.BaudRate = bauds;
+                _serialPort.DataBits = 8;
+                _serialPort.StopBits = StopBits.One;
+
+                //TODO: Add delegate for received data
+                return true;
+            }
+            return false;
+            
+        }
+
+        public bool IsOpen()
+        {
+            return _serialPort.IsOpen;
+        }
+
+        public void Close()
+        {
+            if (IsOpen())
+            {   //TODO: Remove delegate for received data before closing port
+                _serialPort.Close();
+            }
+            
+        }
     }
 }
